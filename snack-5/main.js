d0a10ff7a9b7a1e22a0099c59caa89eed9aca2cc
GioMench <giorgiaglm@gmail.com>
2024-02-26 12:39:50 +0100

    snack-5

--- snack-5/main.js
+++ snack-5/main.js
@@ -3,3 +3,12 @@ Stampa il cubo dei primi N numeri,
 dove N è un numero indicato dall’utente.
 */
 
+let numb = Number(prompt('inserisci un numero'));
+ console.log(numb);
+
+
+
+let numbCube = Math.pow(numb , 3);
+ console.log(numbCube);
+
+
